75dace9515523111a6eafb5468426c8c080ff722
CJewon <112820744+CJewon@users.noreply.github.com>
2024-05-20 15:39:08 +0900

    리스트 데이터 추가 및 최신순, 평점순 정렬버튼 생성, 삭제버튼 생성 및 기능구현 완료

---
+++ src/Components/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ReviewList from "./ReviewList";
+import mockItems from "../mock.json";
+
+export default function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  const handleNewestClick = () => setOrder("createdAt");
+  const handleBestClick = () => setOrder("rating");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={handleNewestClick}>최신순</button>
+        <button onClick={handleBestClick}>베스트순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete}></ReviewList>
+    </div>
+  );
+}
