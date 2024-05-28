3e198627bd8e68d22b119f0a0786313f6e11483a
CJewon <112820744+CJewon@users.noreply.github.com>
2024-05-28 13:41:12 +0900

    useRef를 이용해 참조해야할 부분 지정하기, ReviewForm 컴포넌트 내부 state 정리

---
+++ src/Components/FileInput.js
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+
+export default function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
+
+  const handleChange = (e) => {
+    const nextValue = e.target.files[0];
+    onChange(name, nextValue);
+  };
+
+  const handleCleraClick = () => {
+    const inputNode = inputRef.current;
+    if (!inputNode) return;
+
+    inputNode.value = "";
+    onChange(name, null);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleChange} ref={inputRef} />
+      {value && <button onClick={handleCleraClick}></button>}
+    </div>
+  );
+}
--- src/Components/ReviewForm.js
+++ src/Components/ReviewForm.js
@@ -1,30 +1,53 @@
-import React, { useState } from 'react'
-import './ReviewForm.css'
+import React, { useState } from "react";
+import "./ReviewForm.css";
+import FileInput from "./FileInput";
 
 export default function ReviewForm() {
-    const [title, setTitle] = useState('')
-    const [rating, setRating] = useState(0)
-    const [content, setContent] = useState('');
+  const [values, setValues] = useState({
+    title: "",
+    rating: 0,
+    content: "",
+    imgFile: null,
+  });
 
+  const handleChange = (name, value) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
 
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
 
-    const   handleTitleChange = (e) => {
-        setTitle(e.target.value);
-    }
-
-    const handleRatingChange = (e) => {
-        const nextRating = Number(e.target.value) || 0;
-        setRating(nextRating)
-    }
-
-    const handleContentChange = (e) => {
-        setContent(e.target.value)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
   return (
-    <form className='ReviewForm'>
-        <input value={title} onChange={handleTitleChange} />
-        <input type="number" value={rating} onChange={handleRatingChange} />
-        <textarea value={content} onChange={handleContentChange}></textarea>
+    <form className="ReviewForm">
+      <FileInput
+        name="imgFile"
+        value={values.imgFile}
+        onChange={handleChange}
+      ></FileInput>
+      <input name="title" value={values.title} onChange={handleInputChange} />
+      <input
+        name="rating"
+        type="number"
+        value={values.rating}
+        onChange={handleInputChange}
+      />
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      ></textarea>
+      <button type="submit" onClick={handleSubmit}>
+        확인
+      </button>
     </form>
-  )
+  );
 }
